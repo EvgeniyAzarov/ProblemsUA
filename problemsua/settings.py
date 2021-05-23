f121ab92a6b4602a2f3407f5a9117af72b644a85
Evgeniy Azarov <azarov.evg.ua@gmail.com>
2021-05-23 21:57:16 +0300

    Add bootstrap

--- problemsua/settings.py
+++ problemsua/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'problemsua.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,4 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static-root')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
