1f9ca0d6404d2d7c9a343c965090050cdb01dabe
Evgeniy Azarov <azarov.evg.ua@gmail.com>
2021-05-28 18:20:43 +0300

    Add page for single problem

--- problems/urls.py
+++ problems/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('problem/<int:problem_id>', views.problem, name="problem"),
     path('problems/', views.problems_list, name="problems_list"),
 ]
--- problems/views.py
+++ problems/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Problem
 
@@ -16,5 +16,13 @@ def problems_list(request, per_page=10):
 
     context = {'page_obj': page_obj}
     return render(request,
-                  template_name='problems/index.html',
+                  template_name='problems/problems_list.html',
+                  context=context)
+
+
+def problem(request, problem_id):
+    pr = get_object_or_404(Problem, pk=problem_id)
+    context = {'problem': pr}
+    return render(request,
+                  template_name='problems/problem.html',
                   context=context)
