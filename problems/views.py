ed820ba9f7b727921fe87d11178c01d10a664f20
Evgeniy Azarov <azarov.evg.ua@gmail.com>
2021-06-02 10:29:12 +0300

    Fix filter duplications bug

--- problems/views.py
+++ problems/views.py
@@ -17,24 +17,22 @@ def problems_list(request, per_page=10):
     if search_text:
         search_text.strip()
         context['search_text'] = search_text
-        query.add(Q(text__icontains=search_text) | Q(name__contains=search_text), Q.AND)
+        query &= (Q(text__icontains=search_text) | Q(name__contains=search_text))
 
     source_id = request.GET.get('source_id')
     if source_id and source_id != "0":
         source_id = int(source_id)
         filter_context['selected_source_id'] = source_id
-        query.add(Q(source__id=source_id), Q.AND)
+        query &= Q(source__id=source_id)
 
     filter_context['selected_attributes'] = list(Attribute.objects.all().values_list('id', flat=True))
     selected_attributes = request.GET.getlist('selected_attributes', None)
     if selected_attributes:
         selected_attributes = list(map(int, selected_attributes))
-        query.add(Q(attributes__in=selected_attributes), Q.AND)
+        query &= Q(attributes__in=selected_attributes)
         filter_context['selected_attributes'] = selected_attributes
 
-    print(filter_context['selected_attributes'])
-
-    problems = Problem.objects.filter(query).order_by('id')
+    problems = Problem.objects.filter(query).distinct().order_by('id')
     paginator = Paginator(problems, per_page)
 
     page_number = request.GET.get('page')
