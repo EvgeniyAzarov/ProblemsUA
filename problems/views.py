c478c4a2ffd7ec68a9bcbb3c060896f0d2b2475d
Evgeniy Azarov <azarov.evg.ua@gmail.com>
2021-06-02 04:01:31 +0300

    Add attributes filter

--- problems/views.py
+++ problems/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 from django.db.models import Q
-from .models import Problem, Source
+from .models import Problem, Source, Attribute
 
 
 def index(request):
@@ -11,22 +11,30 @@ def index(request):
 def problems_list(request, per_page=10):
     context = {}
     filter_context = {}
-    problems = Problem.objects
+    query = Q()
 
     search_text = request.GET.get('search_text')
     if search_text:
         search_text.strip()
         context['search_text'] = search_text
-        problems = problems \
-            .filter(Q(text__icontains=search_text) | Q(name__contains=search_text))
+        query.add(Q(text__icontains=search_text) | Q(name__contains=search_text), Q.AND)
 
     source_id = request.GET.get('source_id')
     if source_id and source_id != "0":
         source_id = int(source_id)
         filter_context['selected_source_id'] = source_id
-        problems = problems.filter(source__id=source_id)
+        query.add(Q(source__id=source_id), Q.AND)
 
-    problems = problems.order_by('id')
+    filter_context['selected_attributes'] = list(Attribute.objects.all().values_list('id', flat=True))
+    selected_attributes = request.GET.getlist('selected_attributes', None)
+    if selected_attributes:
+        selected_attributes = list(map(int, selected_attributes))
+        query.add(Q(attributes__in=selected_attributes), Q.AND)
+        filter_context['selected_attributes'] = selected_attributes
+
+    print(filter_context['selected_attributes'])
+
+    problems = Problem.objects.filter(query).order_by('id')
     paginator = Paginator(problems, per_page)
 
     page_number = request.GET.get('page')
@@ -36,6 +44,8 @@ def problems_list(request, per_page=10):
 
     sources = Source.objects.all()
     filter_context['sources'] = sources
+    attributes = Attribute.objects.all()
+    filter_context['attributes'] = attributes
 
     context['filter'] = filter_context
 
@@ -57,4 +67,4 @@ def compile_paper(request):
 
 
 def test_page(request):
-     pass
+    pass
