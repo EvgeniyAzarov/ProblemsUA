d4da22d267e83bcbe95eee54c7390745a361a8fc
Evgeniy Azarov <azarov.evg.ua@gmail.com>
2021-06-07 01:56:47 +0300

    Fix static directory

--- problemsua/settings.py
+++ problemsua/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'problems/static')
 ]
 
 # Heroku: Update database configuration from $DATABASE_URL.
